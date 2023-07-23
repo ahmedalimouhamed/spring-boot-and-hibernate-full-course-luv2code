2d96f7c4eb3430ffb2d325e0c7c41f9a0e8c5459
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2023-07-23 20:28:25 +0100

    bean lifeccycle methods

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,28 @@
+package com.luv2code.springcoredemo.common;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+    public CricketCoach() {
+        System.out.println("in constructor : "+getClass().getSimpleName());
+    }
+
+    //init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff(): "+ getClass().getSimpleName());
+    }
+
+    //destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff(): "+ getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
