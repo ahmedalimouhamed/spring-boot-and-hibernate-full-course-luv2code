a8547ef20c6d01304777f074f8aae3c94267d027
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2023-07-24 06:02:50 +0100

    cruddemosudent 2

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDao;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +15,30 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
+	public CommandLineRunner commandLineRunner(StudentDao studentDao){
 		return runner -> {
-			System.out.println("Hello world");
+			//createStudent(studentDao);
+			createMultipleStudents(studentDao);
 		};
+	}
+
+	private void createMultipleStudents(StudentDao studentDao) {
+		System.out.println("Creating new student object ...");
+		Student tempStudent1 = new Student("paul", "doe", "paul@doe.com");
+		Student tempStudent2 = new Student("ahmed", "ali", "ahmed@ali.com");
+		Student tempStudent3 = new Student("amina", "alami", "amina@alami.com");
+
+		System.out.println("Saving the students...");
+		studentDao.save(tempStudent1);
+		studentDao.save(tempStudent2);
+		studentDao.save(tempStudent3);
+	}
+
+	private void createStudent(StudentDao studentDao) {
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("paul", "doe", "paul@doe.com");
+		System.out.println("Saving the student...");
+		studentDao.save(tempStudent);
+		System.out.println("saved student. Generated id : "+tempStudent.getId());
 	}
 }
---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDao.java
@@ -0,0 +1,7 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+
+public interface StudentDao {
+    public void save(Student theStudent);
+}
