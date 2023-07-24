51aa1b8a21ebbfecb4c3b23925579366bd7e10f0
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2023-07-24 16:34:03 +0100

    spring boot rest crud employee 1

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.luv2code.springboot.cruddemo.service;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeDao;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    private EmployeeDao employeeDao;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDao.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDao.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEployee) {
+        return employeeDao.save(theEployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDao.deleteById(theId);
+    }
+}
